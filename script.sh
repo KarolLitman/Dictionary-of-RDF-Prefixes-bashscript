d6b22792917b99cb04069762dda1fc7674187e6d
Karol <karolks94@gmail.com>
2018-06-03 22:30:14 +0200

    Initial commit

---
+++ script.sh
@@ -0,0 +1,63 @@
+function curlGetBodyAndCode (){
+
+
+# store the whole response with the status at the and
+HTTP_RESPONSE=$(curl --silent --write-out "HTTPSTATUS:%{http_code}" -F file=@$2 -X POST -H 'Accept: text/turtle' $1)
+
+# echo "$HTTP_RESPONSE"
+
+# extract the body
+HTTP_BODY=$(echo "$HTTP_RESPONSE" | sed -e 's/HTTPSTATUS\:.*//g')
+
+# extract the status
+HTTP_STATUS=$(echo "$HTTP_RESPONSE" | tr -d '\n' | sed -e 's/.*HTTPSTATUS://')
+
+# print the body
+
+# example using the status
+if [ ! $HTTP_STATUS -eq 200  ]; then
+  echo -e "Endpoint: $1\nError HTTP Code: $HTTP_STATUS"
+  exit 1
+
+else
+
+newfilename="$filename"_name.ttl
+
+  echo -e "Endpoint: $1\nCreated new file: $newfilename"
+  echo "$HTTP_BODY">"$newfilename"
+goodresponse=true
+fi
+}
+
+filename=`basename $1 .ttl`
+
+result2=$(grep -v -i '@prefix_endpoint' $1)
+
+tmpfile=$(mktemp)
+
+echo "$result2">"$tmpfile"
+
+
+
+
+
+# echo "$result2"
+
+result=($(grep -i -o -P '(?<=@prefix_endpoint <).*(?=>)' $1))
+
+for i in "${result[@]}"
+do
+
+if [ "$goodresponse" = true ] ; then
+    break;
+fi
+
+   curlGetBodyAndCode $i $tmpfile
+done
+
+
+rm "$tmpfile"
+
+
+
+
